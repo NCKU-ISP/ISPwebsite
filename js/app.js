595cd9e95e1a1e5cac831f7a1298ccf3390ecc39
William Chang <wilicw1346@gmail.com>
2022-01-26 12:12:42 +0800

    New version website build via Vuejs

--- js/app.js
+++ js/app.js
@@ -1,168 +1,146 @@
-var md = window.markdownit();
+const md = window.markdownit();
 
-(function () {
-    /* Regular expression, please see here for more info
-     * https://docs.microsoft.com/zh-tw/dotnet/api/system.text.regularexpressions.regex?view=net-5.0
-     */
-    const md_rule = {
-        'tab': /[\r\n]---[\r\n]/,
-        'section': /[\r\n]----[\r\n]/,
-        'identifier1': /#\s\S+\s/,
-        'identifier2': /##\s\S+\s/,
-        'identifier3': /###\s\S+\s/,
-        'title_filter1': /#\s\S+/,
-        'title_filter2': /##\s.+/,
-    }
+/**
+ * Regular expression, please see here for more info
+ * https://docs.microsoft.com/zh-tw/dotnet/api/system.text.regularexpressions.regex?view=net-5.0
+*/
+const md_rule = {
+    tab: /[\r\n]---[\r\n]/g,
+    section: /[\r\n]----[\r\n]/g,
+    identifier1: /#\s\S+\s/g,
+    identifier2: /##\s\S+\s/g,
+    identifier3: /###\s\S+\s/g,
+    title_filter1: /[^#]#\s\S+/g,
+    title_filter2: /[^#]##\s.+/g,
+    list: /-\s[^\[\]]\S.+/g,
+    link: /-\s\[\S.+\]\(\S.+\)/g,
+    link_text: /\[\S.+\]/g,
+    link_url: /\(\S.+\)/g,
+}
 
-    function set_tab_display(intro_tag, content_tag, id) {
-        /* Set the click event of switching pages
+/**
+  * Generate the navbar items and bind the clicking event
+  * @param content {string} The article to be analyzed.
+*/
+function navigator_generator(content) {
+    let navbar_items = $('#navbar_items')
+    let btn_title = content.match(md_rule.identifier1)[0].slice(2, -1);
+    let dropdown_menu = []
+    let sub_titles = [...(content.matchAll(md_rule.title_filter2) || [])].flat()
 
-        Args:
-            intro_tag: the id of button to be trigger.
-            content_tag: the id of content to be show.
-            id: the identifying number of the content.
-         */
-        var count = $(content_tag).children('div').length
-        for (i = 0; i < count; i++) {
-            $(content_tag + ' #tab-' + i).css('display', (i == id) ? 'block' : 'none')
-            if (i == id) $(intro_tag + ' #tab-button-' + i).addClass('active')
-            else $(intro_tag + ' #tab-button-' + i).removeClass('active')
-        }
-    }
-
-    function article_parser(contents, intro_tag, content_tag) {
-        sections = contents.split(md_rule['section'])
-
-        sections.forEach((article, idx) => {
-            // headers
-            if (idx == 0) {
-                content_render(article, intro_tag)
-                $(intro_tag).append(
-                    $('<div class="tab-button-container"></div>'))
-            }
-            // articles
-            else {
-                title = article.match(md_rule['title_filter2'])[0].slice(2, -1);
-                tab_btn_container = intro_tag + ' div'
-                $(tab_btn_container).append(
-                    $('<button class="tab-links"></button>')
-                        .on('click', function () { set_tab_display(intro_tag, content_tag, idx - 1) })
-                        .attr('id', 'tab-button-' + (idx - 1))
-                        .text(title)
-                )
-                $(content_tag).append(
-                    $('<div class="md-container"></div>')
-                        .attr('id', 'tab-' + (idx - 1))
-                        .append(md.render(article))
-                )
-            }
-        })
-
-        // set default to display
-        set_tab_display(intro_tag, content_tag, 0)
-    }
-
-    function content_render(content, selector) {
-        $(selector).append(md.render(content))
-    }
-
-    function navigator_generator(nav_tag, content, intro_tag, content_tag) {
-        /* Generate the navbar items and bind the clicking event
-
-        Args: 
-            nav_tag: the id of the navbar container. The navbar content will be append in it.
-            content: the article to be analyzed.
-            intro_tag: the tag of the container of the tab header.
-            content_tag: the tag of the container of the content.
-         */
-        btn_title = content.match(md_rule['identifier1'])[0].slice(2, -1);
-        var sub_btn = $('<ul></ul>')
-
-        sub_content = content.split(md_rule['section'])
-        sub_content.forEach((sub, idx) => {
-            console.log(sub)
-            sub_title = sub.match(md_rule['title_filter2'])
-            if (sub_title != null)
-                sub_btn.append(
-                    $('<li></li>').append(
-                        $('<a></a>')
-                            //.attr('href', '#sub-' + get_page_id(sub))
-                            .on('click', function () { set_tab_display(intro_tag, content_tag, idx - 1) })
-                            .attr('href', '#btn-' + btn_title)
-                            .text(sub_title[0].slice(2, -1))
-                    )
-                )
-        })
-        $(nav_tag).append(
-            $('<li></li>').append(
-                $('<div></div>')
-                    .append(
-                        $('<a></a>')
-                            .attr('href', '#btn-' + btn_title)
-                            .text(btn_title)
-                    )
-                    .append(sub_btn)
-            )
+    sub_titles.forEach(sub_title => {
+        sub_title = sub_title.slice(3).trim()
+        dropdown_menu.push(
+            $(`
+                <li>
+                    <a class="dropdown-item" href="#${sub_title}">${sub_title}</a>
+                </li>
+            `)
         )
+    })
+
+    let main_item = null
+
+    if (dropdown_menu.length == 0) {
+        main_item = $(`
+            <li class="nav-item">
+                <a class="nav-link mx-2" href="${'#'}">${btn_title}</a>
+            </li>
+        `)
+    } else {
+        let menu = $(`<ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="nav_1"></ul>`)
+        dropdown_menu.forEach(o => menu.append(o))
+        main_item = $(`
+            <li class="nav-item dropdown">
+                <a
+                    class="nav-link mx-2 dropdown-toggle"
+                    href="${'#'}"
+                    id="nav_1"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                >
+                    ${btn_title}
+                </a>
+            </li>
+        `).append(menu)
     }
 
-    function get_page_id(article) {
-        return 'btn-' + article.match(md_rule['identifier1'])[0].slice(2, -1)
+    navbar_items.append(main_item)
+}
+
+var tab_handler = {
+    'NCKU': _ => null,
+    '團隊介紹': contents => {
+        let contents_dom = $(md.render(contents))
+        const main_div = $(`<div id="team"></div>`)
+        navigator_generator(contents)
+        contents_dom.filter("h2").each(function () {
+            console.log($(this).attr("id", $(this).text()));
+        })
+        $('#main').append(main_div.append(contents_dom))
+    },
+    '各組介紹': contents => {
+
+    },
+    '活動': contents => {
+        navigator_generator(contents)
+    },
+    '專案計畫': function (contents) {
+        navigator_generator(contents)
+    },
+    '歷屆作品': contents => {
+        navigator_generator(contents)
+    },
+    '聯絡方式': contents => {
+        const addresses = [...contents.matchAll(md_rule.list)].flat()
+        const link_div = $(`<div class="row justify-content-center pt-3"></div>`)
+        const links = [...contents.matchAll(md_rule.link)].flat()
+
+        addresses.forEach(i => {
+            $('#contact')
+                .append(
+                    $(`
+                            <p class="text-center text-white">
+                                <small>${i.slice(2)}</small>
+                            </p>
+                        `)
+                )
+        })
+
+        links.forEach(ll => {
+            const text = ll.match(md_rule.link_text).pop().slice(1, -1);
+            const url = ll.match(md_rule.link_url).pop().slice(1, -1);
+            link_div.append(
+                $(`
+                    <p class="col-1 text-center">
+                        <a
+                            href="${url}"
+                            target="_blank"
+                            class="text-white"
+                        >
+                            <i class="bi bi-${text}"></i>
+                        </a>
+                    </p>
+                `)
+            )
+        })
+
+        $('#contact').append(link_div)
     }
+}
 
-    var tab_handler = {
-        'NCKU': (contents) => { },
-        '團隊介紹': (contents) => {
-            $('#page-team').attr('id', get_page_id(contents))
-            navigator_generator('#navigator-container', contents)
-            content_render(contents, '#content-team')
-        },
-        '各組介紹': (contents) => {
-            //$('#content-team').attr('id', contents.match(md_rule['identifier2']))
-            sections = contents.split(md_rule['section'])
-            // headers
+$.get('./data/content.md', data => {
+    const tabs = data.split(md_rule.tab)
+    tabs.forEach(tab_content => {
+        let id = tab_content.match(md_rule.identifier1)[0].slice(2, -1);
 
-            // acedamic
-
-            // administration
-
-        },
-        '活動': function (contents) {
-            $('#page-activities').attr('id', get_page_id(contents))
-            navigator_generator('#navigator-container', contents, '#activities-intro', '#activities-articles')
-            article_parser(contents, '#activities-intro', '#activities-articles')
-        },
-        '專案計畫': function (contents) {
-            $('#page-projects').attr('id', get_page_id(contents))
-            navigator_generator('#navigator-container', contents, '#projects-intro', '#projects-articles')
-            article_parser(contents, '#projects-intro', '#projects-articles')
-        },
-        '歷屆作品/商品': (contents) => {
-            $('#page-products').attr('id', get_page_id(contents))
-            navigator_generator('#navigator-container', contents, '#products-intro', '#products-articles')
-            article_parser(contents, '#products-intro', '#products-articles')
-        },
-        '聯絡我們': (contents) => {
-            $('#page-contact').attr('id', get_page_id(contents))
-            navigator_generator('#navigator-container', contents)
-            content_render(contents, '#content-contact')
+        try {
+            tab_handler[id](tab_content)
+        } catch (e) {
+            console.log(e)
         }
-    }
-
-    $.get('./data/content.md', function (data) {
-        var tabs = data.split(md_rule['tab'])
-        tabs.forEach((tab_content, idx) => {
-            console.log(idx)
-            id = tab_content.match(md_rule['identifier1'])[0].slice(2, -1);
-            console.log(id)
-
-            try {
-                tab_handler[id](tab_content)
-            }
-            catch (e) {
-                console.log(e)
-            }
-        });
     });
-})()
+});
 
+$("#year").text((new Date()).getFullYear())
